fc74de2a1a6bc7e146b498a56c6e8eeff446a722
Dennis <denkovarik123@gmail.com>
2025-05-04 19:01:17 -0400

    Created script to export API keys from config file

---
+++ setup/export_api_keys.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Export the API keys
+config_file="config/config.ini"
+
+# Read the API keys from the config file
+openai_api_key=$(awk -F '=' '/^OPENAI_API_KEY/ {print $2}' "$config_file" | tr -d ' ');
+grok_api_key=$(awk -F '=' '/^XAI_API_KEY/ {print $2}' "$config_file" | tr -d ' ');
+
+# Export the keys
+export OPENAI_API_KEY="$openai_api_key"
+export XAI_API_KEY="$openai_api_key"
