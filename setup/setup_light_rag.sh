f7cff1f992e54754b400cfcb49ad0de05e4d4050
Dennis <denkovarik123@gmail.com>
2025-05-04 19:01:51 -0400

    Created script to setup light rag.

---
+++ setup/setup_light_rag.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+original_dir=$(pwd)
+
+echo "Current working directory: $(pwd)";
+
+cd "$HOME";
+
+echo "Current working directory: $(pwd)";
+
+git clone https://github.com/HKUDS/LightRAG.git
+
+cd LightRAG
+
+pip install -e .
+
+cd "$original_dir";
+
+echo "Current working directory: $(pwd)";
