b40bd7e9216004071ff516125f0358661a164c81
Dennis <denkovarik123@gmail.com>
2025-05-06 17:45:56 -0400

    Added demo scripts and modified to read sample chat

---
+++ testing/LightRAG/lightrag_ollama_demo.py
@@ -0,0 +1,115 @@
+import asyncio
+import nest_asyncio
+
+import os
+import inspect
+import logging
+from lightrag import LightRAG, QueryParam
+from lightrag.llm.ollama import ollama_model_complete, ollama_embed
+from lightrag.utils import EmbeddingFunc
+from lightrag.kg.shared_storage import initialize_pipeline_status
+import time
+
+nest_asyncio.apply()
+
+WORKING_DIR = "./LightRAG_Ollama"
+
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
+
+if not os.path.exists(WORKING_DIR):
+    os.mkdir(WORKING_DIR)
+
+
+async def initialize_rag():
+    rag = LightRAG(
+        working_dir=WORKING_DIR,
+        llm_model_func=ollama_model_complete,
+        llm_model_name="qwen3:latest",
+        llm_model_max_async=4,
+        llm_model_max_token_size=32768,
+        llm_model_kwargs={
+            "host": "http://localhost:11434",
+            "options": {"num_ctx": 32768},
+        },
+        embedding_func=EmbeddingFunc(
+            embedding_dim=768,
+            max_token_size=8192,
+            func=lambda texts: ollama_embed(
+                texts, embed_model="nomic-embed-text", host="http://localhost:11434"
+            ),
+        ),
+    )
+
+    await rag.initialize_storages()
+    await initialize_pipeline_status()
+
+    return rag
+
+
+async def print_stream(stream):
+    async for chunk in stream:
+        print(chunk, end="", flush=True)
+
+
+def main():
+    # Initialize RAG instance
+    rag = asyncio.run(initialize_rag())
+
+    # Insert example text
+    with open("./chats/llm_sample_chat.json", "r", encoding="utf-8") as f:
+        rag.insert(f.read())
+        
+    query="Can you please summarize this chat?"
+
+    # Test different query modes
+    print("\nNaive Search:")
+    print(
+        rag.query(
+            query, param=QueryParam(mode="naive")
+        )
+    )
+
+    print("\nLocal Search:")
+    print(
+        rag.query(
+            query, param=QueryParam(mode="local")
+        )
+    )
+
+    print("\nGlobal Search:")
+    print(
+        rag.query(
+            query, param=QueryParam(mode="global")
+        )
+    )
+
+    # Start timing
+    start_time = time.time()
+    
+    print("\nHybrid Search:")
+    print(
+        rag.query(
+            query, param=QueryParam(mode="hybrid")
+        )
+    )
+    
+    # End timing
+    end_time = time.time()
+
+    # Calculate and print the time taken
+    print(f"Operation took {end_time - start_time} seconds")
+
+    # stream response
+    resp = rag.query(
+        "What are the top themes in this story?",
+        param=QueryParam(mode="hybrid", stream=True),
+    )
+
+    if inspect.isasyncgen(resp):
+        asyncio.run(print_stream(resp))
+    else:
+        print(resp)
+
+
+if __name__ == "__main__":
+    main()
