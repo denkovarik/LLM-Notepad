892b1739537b0e322868ff9285cacef2170b7175
Dennis <denkovarik123@gmail.com>
2025-05-14 12:45:18 -0400

    Added print statement to indicate when Light RAG initialization has started

--- main.py
+++ main.py
@@ -298,6 +298,8 @@ def set_lightRAG_embed_model(selection: ModelSelection, request: Request):
     
 @app.post("/api/initialize_light_rag") 
 def init_light_rag(request: Request):
+    print("Initializing Light RAG")
+    
     st = request.app.state.state
     
     # Logic 
@@ -305,7 +307,7 @@ def init_light_rag(request: Request):
     
     message = {"detail": f"Light RAG has been initialized with LLM model set to {st.chat.lightRAG_llm_model_name} and the Embed model set to {st.chat.lightRAG_embed_model_name}"}
     print(message)
-    
+        
     return message
 
 def load_model(model_name: str, request: Request):
